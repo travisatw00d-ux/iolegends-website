0825bc7bbfb67d7db5843c6456dbad0de88aa5f6
travisatw00d-ux <travisatw00d-ux@users.noreply.github.com>
2026-06-20 17:01:29 -0400

    Revert v=2 cache busting, clean deploy trigger

--- public/holdyourground/modules/render.js
+++ public/holdyourground/modules/render.js
@@ -13,10 +13,10 @@ const swordImg = new Image(); swordImg.src = '/images/woodensword.png';
 const zombieHeadImg = new Image(); zombieHeadImg.src = '/images/zombiehead.png';
 const zombieT2HeadImg = new Image(); zombieT2HeadImg.src = '/images/T2zombiehead.png';
 const serverLevelImg = new Image(); serverLevelImg.src = '/images/ServerLevel.png';
-const zombieLeftHandImg = new Image(); zombieLeftHandImg.src = '/images/zombielefthand.png?v=2';
-const zombieRightHandImg = new Image(); zombieRightHandImg.src = '/images/zombierighthand.png?v=2';
-const zombieT2LeftHandImg = new Image(); zombieT2LeftHandImg.src = '/images/T2zombielefthand.png?v=2';
-const zombieT2RightHandImg = new Image(); zombieT2RightHandImg.src = '/images/T2zombierighthand.png?v=2';
+const zombieLeftHandImg = new Image(); zombieLeftHandImg.src = '/images/zombielefthand.png';
+const zombieRightHandImg = new Image(); zombieRightHandImg.src = '/images/zombierighthand.png';
+const zombieT2LeftHandImg = new Image(); zombieT2LeftHandImg.src = '/images/T2zombielefthand.png';
+const zombieT2RightHandImg = new Image(); zombieT2RightHandImg.src = '/images/T2zombierighthand.png';
 
 export function resizeViewport(w, h) {
   state.viewW = w;
